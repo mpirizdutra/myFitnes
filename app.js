626aa1a58ed9733c96134821d6bf2ae7ecb21aae
mpirizdutra89 <mpirizdutra@gmail.com>
2026-03-15 23:30:09 -0300

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(basicAuth({
 }));
 app.use('/', trainingRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8100;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en puerto ${PORT}`);
 });
